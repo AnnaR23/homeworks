0d451e4256751212fc95e43f1b415f5f0265b3fc
AnnaR <mash1977@ukr.net>
2025-01-17 01:02:39 +0100

    add bonus_h_18

---
+++ bonus_homework_18_arrays/script.js
@@ -0,0 +1,41 @@
+console.log('Arrays and Objects');
+
+'use strict';
+
+
+//array
+let room = ['cupboard', 'bed', 'table', 'chair'];
+console.log(room);
+console.log(room[3]);
+console.log(room[0]);
+
+//array
+let flowers = ['Kalanchoe', 'Orchid', 'Begonia', 'Cyclamen'];
+console.log(flowers);
+console.log(flowers[1]);
+console.log(flowers[2]);
+
+
+
+//object
+let picture = {
+    name: 'Farben des Leben',
+    author: 'Anna Roda',
+    style: 'abstraction',
+    width: 70,
+    height: 45,
+}
+console.log(picture.name);
+console.log(picture['width']);
+console.log(picture.style);
+
+//object
+let cupboard = {
+    doors: 3,
+    mirror: 1,
+    shelves: 5,
+    rod: 1,
+}
+console.log(cupboard.doors);
+console.log(cupboard.mirror);
+console.log(cupboard.rod);
