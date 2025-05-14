d618f2572460b5db296481db864b55a465e19590
AnnaR <mash1977@ukr.net>
2025-05-14 03:14:57 +0200

    homework_48

---
+++ homework_48_Material_UI/material-ui-table/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import DataTable from './components/DataTable.jsx'
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>User table</h1>
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
