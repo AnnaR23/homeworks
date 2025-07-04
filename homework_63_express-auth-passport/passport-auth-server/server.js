09fb8ccd6fd6bd88273eaa0527b24872fb2dfe96
AnnaR <mash1977@ukr.net>
2025-07-04 22:03:59 +0200

    homework_63_express-auth-passport

---
+++ homework_63_express-auth-passport/passport-auth-server/server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+import './config/passport.js'; // настройка паспорта
+
+dotenv.config(); // загружаем переменные из .env
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middleware - распарсиваем JSON и формы из запросов
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// настраиваем сессии - способ сохранять, кто сейчас "вошел"
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secret', // секрет для подписи куки
+  resave: false, // не сохраняет сессию в хранилище, если в ней ничего не изменилось
+  saveUninitialized: false, // не сохраняет пустую сессию, если пользователь ничего не сделал
+  cookie: {
+    httpOnly: true, // нельзя прочитать через в JavaScript браузере(защита)
+    secure: false // работает даже без HTTPS (true только если HTTPS)
+  }
+}));
+
+// подключаем Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// подключаем маршруты
+app.use('/auth', authRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('<h2>Welcome to the homeworks page! Use POST /auth/register , POST /auth/login, POST /auth/logout and GET /protected to work with authorization.</h2>');
+})
+
+
+// защищенный маршрут, доступен только после входа
+app.get('/protected', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send(`Welcome, ${req.user.email}! This is protected content.`);
+  } else {
+    res.status(401).send('No access');
+  }
+});
+
+// запускаем сервер
+ app.listen(PORT, () => {
+   console.log(`Server started on port ${PORT}`);
+ });
